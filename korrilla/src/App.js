195534346f3ff244cc4ffc25124b41106b1c8764
Mike Gustafson <gustafson.m@gmail.com>
2025-02-26 19:56:38 -0800

    refactor: remove unused useEffect import in App component

--- korrilla/src/App.js
+++ korrilla/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Receipt from './components/Receipt';
 import receiptsData from './data/receipts';
